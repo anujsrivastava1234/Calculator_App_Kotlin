314c6ee82fe8b0d7f415ee4b9b96e65b8853a223
Anuj Srivastava <Anuj Srivastava>
2024-08-11 20:45:07 +0530

    Button implementation is completed

--- app/src/main/java/st/tutorial/mycalculator/MainActivity.kt
+++ app/src/main/java/st/tutorial/mycalculator/MainActivity.kt
@@ -2,6 +2,8 @@ package st.tutorial.mycalculator
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,8 +22,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        tvInput = findViewById(R.id.tv_input)
     }
     fun onDigit(view: View){
-        Toast.makeText(this,"Button clicked", Toast.LENGTH_SHORT).show()
+        tvInput?.append((view as Button).text)
     }
 }
